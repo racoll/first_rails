ccf3005cd639527b0ee4fc885c60b9ed1cb97f52
robincollins <racollins1@me.com>
2017-10-16 16:27:54 +0100

    show articles working

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,25 @@
 class ArticlesController < ApplicationController
 
+  def show
+    @article = Article.find(params[:id]) # we usa an instance variable to hold a reference to the article object
+  end
+
   def new
   end
 
   def create
-    render plain: params[:article].inspect  # params array containing either get or post request variables, .inspect just shows you the structure.
+    @article = Article.new(article_params)
+    # params array containing either get or post request variables, .inspect just shows you the structure.
+    # referring to the class named Article defined in app/models/article.rb
+    @article.save
+    redirect_to @article
+
   end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
   # The params method is the object which represents the parameters (or fields) coming in from the form
 
 end
